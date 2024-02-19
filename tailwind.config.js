fe3e4e01b576ab996d1ea2ef87cb81fd430ab100
hendabdelnasser <105015474+hendabdelnasser@users.noreply.github.com>
2024-02-19 21:39:35 +0200

    first

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily:{
+        customFont: ['"Ping AR + LT"',"sans-serif"
+
+        ]
+      }
+    },
+  },
+  plugins: [],
+}
+
